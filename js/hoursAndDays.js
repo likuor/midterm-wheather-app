5eb8bc413eb03a752475fceaf0b1a59aa809c77f
Koki Sakai <likuor0528@gmail.com>
2022-06-13 11:42:49 -0700

    TEST

--- js/hoursAndDays.js
+++ js/hoursAndDays.js
@@ -1,6 +1,7 @@
 // import config from '../apikey.js';
 // const WEATHER_API_KEY = config.wheatherApi;
 console.log(WEATHER_API_KEY);
+console.log(process.env.WEATHER_API_KEY);
 
 let data;
 let weatherInfo = {
